f81bbba32c1a49320a4512b2cca3557cb260d78d
zhangye <yzhang0711@163.com>
2020-02-04 23:54:02 +0800

    easy 位运算

---
+++ src/leetcode/coding/_389_找不同.java
@@ -0,0 +1,18 @@
+package leetcode.coding;
+
+public class _389_找不同 {
+
+    public char findTheDifference(String s, String t) {
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            arr[t.charAt(i) - 'a']--;
+            if (arr[t.charAt(i) - 'a'] == -1) {
+                return t.charAt(i);
+            }
+        }
+        return 'a';
+    }
+}
