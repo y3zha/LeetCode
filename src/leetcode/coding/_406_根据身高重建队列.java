e21c2ddd2f7acb73d75f58bf1db97946a90a7155
zhangye <yzhang0711@163.com>
2020-02-11 13:09:14 +0800

    比较巧妙的题

---
+++ src/leetcode/coding/_406_根据身高重建队列.java
@@ -0,0 +1,33 @@
+package leetcode.coding;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。
+ * 编写一个算法来重建这个队列。
+
+ */
+public class _406_根据身高重建队列 {
+
+    /**
+     * 解题思路：先排序再插入
+     * 1.排序规则：按照先H高度降序，K个数升序排序
+     * 2.遍历排序后的数组，根据K插入到K的位置上
+     *
+     * 核心思想：高个子先站好位，矮个子插入到K位置上，前面肯定有K个高个子，矮个子再插到前面也满足K的要求
+     */
+
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0]);
+        List<int[]> res = new LinkedList<>();
+        //遍历每个元素
+        for (int[] p : people) {
+            res.add(p[1], p);
+        }
+        return res.toArray(new int[res.size()][2]);
+    }
+
+
+}
