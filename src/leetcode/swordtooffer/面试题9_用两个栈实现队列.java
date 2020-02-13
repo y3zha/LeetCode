f8dcf23fdbc656359a03dd91a64b0cb83a2076df
zhangye <yzhang0711@163.com>
2020-02-13 23:24:11 +0800

    剑指offer

--- src/leetcode/swordtooffer/面试题9_用两个栈实现队列.java
+++ src/leetcode/swordtooffer/面试题9_用两个栈实现队列.java
@@ -2,6 +2,13 @@ package leetcode.swordtooffer;
 
 import java.util.Stack;
 
+/**
+ * 利用两个栈实现队列
+ * stack1作为出队的时候用
+ * stack2作为push的时候用，push都是push到stack2中去
+ * 思路：我们总是把元素push到stack2中去，要出队的时候，如果stack1为空，就把stack2的都放到stack1中去，再stack1出队
+ * 否则stack就可以直接出队
+ */
 public class 面试题9_用两个栈实现队列 {
 
     class CQueue {
