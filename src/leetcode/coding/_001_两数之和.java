edb5fcf8424f5fc43e19d668ca3a6215721ce965
zhangye <yzhang0711@163.com>
2020-02-19 14:49:46 +0800

    DFS 回溯

---
+++ src/leetcode/coding/_001_两数之和.java
@@ -0,0 +1,21 @@
+package leetcode.coding;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class _001_两数之和 {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map;
+        map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+        }
+        return new int[]{};
+    }
+}
